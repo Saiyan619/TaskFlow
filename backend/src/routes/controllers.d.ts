fff826f15043fa77ec5299a0c9ed9593d9de689e
saiyan619 <arokoyueb11@gmail.com>
2026-09-04 16:25:43 +0100

    clearing js files

--- backend/src/routes/controllers.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Request, Response } from "express";
-export declare const createTask: (req: Request, res: Response) => Promise<void>;
-export declare const getAllTasks: (req: Request, res: Response) => Promise<void>;
-export declare const getTaskById: (req: Request, res: Response) => Promise<void>;
-export declare const updateTask: (req: Request, res: Response) => Promise<void>;
-export declare const deleteTask: (req: Request, res: Response) => Promise<void>;
-//# sourceMappingURL=controllers.d.ts.map
